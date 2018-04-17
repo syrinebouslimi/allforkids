cec3290131317b7fa0ad8e3f455f2969c19e69fb
Syrine <syrinebouslimi3@gmail.com>
2018-04-17 17:46:11 +0100

    gallerie

--- src/KidsBackendBundle/Controller/AdminEnseignantsController.php
+++ src/KidsBackendBundle/Controller/AdminEnseignantsController.php
@@ -10,4 +10,28 @@ class AdminEnseignantsController extends Controller
     {
         return $this->render('', array('name' => $name));
     }
+
+
+
+    public function afficherAllEnseignantAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $allNotif = $em->getRepository('MgiletNotificationBundle:NotifiableNotification')->findAll();
+
+        $enseignant = $this->getDoctrine()->getRepository('UserBundle:Enseignant')->findAll();
+
+        return $this->render('@KidsBackend/afficherallenseignant.html.twig', array('enseignant' => $enseignant, 'notifiableNotifications' => $allNotif));
+    }
+
+    public function afficherEnseignantbyidAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $allNotif = $em->getRepository('MgiletNotificationBundle:NotifiableNotification')->findAll();
+
+        $enseignant = $this->getDoctrine()->getRepository('UserBundle:Enseignant')->find($id);
+
+        return $this->render('@KidsBackend/detailenseignantparid.html.twig', array('enseignant' => $enseignant, 'notifiableNotifications' => $allNotif));
+    }
+
+
 }
--- src/KidsBackendBundle/Controller/AdminGallerieController.php
+++ src/KidsBackendBundle/Controller/AdminGallerieController.php
@@ -10,4 +10,19 @@ class AdminGallerieController extends Controller
     {
         return $this->render('', array('name' => $name));
     }
+
+    public function afficherAllGallerieAction()
+    {
+        $em=$this->getDoctrine()->getManager();
+
+        $etablis = $em->getRepository('UserBundle:Etablissement')->findAll();
+
+
+        $em = $this->getDoctrine()->getManager();
+        $allNotif = $em->getRepository('MgiletNotificationBundle:NotifiableNotification')->findAll();
+
+        $gallerie = $this->getDoctrine()->getRepository('UserBundle:Gallerie')->findAll();
+
+        return $this->render('@KidsBackend/afficherallgallerie.html.twig', array('gallerie' => $gallerie, 'notifiableNotifications' => $allNotif, 'etab'=>$etablis));
+    }
 }
