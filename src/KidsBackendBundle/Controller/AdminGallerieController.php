f9f48599ed76855f470193c522a260f74fac45b2
Syrine Bouslimi <syrine.bouslimi@esprit.tn>
2018-04-17 22:28:19 +0100

    Admin gallerie filtre

--- src/KidsBackendBundle/Controller/AdminGallerieController.php
+++ src/KidsBackendBundle/Controller/AdminGallerieController.php
@@ -3,6 +3,7 @@
 namespace KidsBackendBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminGallerieController extends Controller
 {
@@ -24,5 +25,57 @@ class AdminGallerieController extends Controller
         $gallerie = $this->getDoctrine()->getRepository('UserBundle:Gallerie')->findAll();
 
         return $this->render('@KidsBackend/afficherallgallerie.html.twig', array('gallerie' => $gallerie, 'notifiableNotifications' => $allNotif, 'etab'=>$etablis));
+    }
+
+    public function afficherGallerieByEtabAction(Request $request)
+    {
+
+
+        $em = $this->getDoctrine()->getManager();
+        $allNotif = $em->getRepository('MgiletNotificationBundle:NotifiableNotification')->findAll();
+
+        $etablis = $em->getRepository('UserBundle:Etablissement')->findAll();
+
+        $option = $request->query->get('idEtab');
+        $gallerie = $this->getDoctrine()->getRepository('UserBundle:Gallerie')->findBy(array(
+            'etablissementId' => $option));
+
+        if ($option != "Voir tout"){
+            return $this->render('@KidsBackend/afficherfiltredgallerie.html.twig', array('gallerie' => $gallerie,  'notifiableNotifications' => $allNotif,'etab'=>$etablis));
+
+        } else {
+           return $this->redirectToRoute('afficherAllGallerie');
+
+
+        }
+
+
+
+
+
+
+//        if (strlen($search) != 0) {
+//            return $this->render('@KidsBackend/afficheralletablissement.html.twig', array('etablissement' => $etablissement,  'notifiableNotifications' => $allNotif));
+//
+//        } else {
+//            return $this->redirectToRoute('afficherAllEtablissement');
+//
+//        }
+
+
+
+
+
+//        $em=$this->getDoctrine()->getManager();
+//
+//        $etablis = $em->getRepository('UserBundle:Etablissement')->findAll();
+//
+//
+//        $em = $this->getDoctrine()->getManager();
+//        $allNotif = $em->getRepository('MgiletNotificationBundle:NotifiableNotification')->findAll();
+//
+//        $gallerie = $this->getDoctrine()->getRepository('UserBundle:Gallerie')->findAll();
+//
+//        return $this->render('@KidsBackend/afficherallgallerie.html.twig', array('gallerie' => $gallerie, 'notifiableNotifications' => $allNotif, 'etab'=>$etablis));
     }
 }
