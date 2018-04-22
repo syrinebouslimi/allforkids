18f25484a478f9528ec40dd4b6b2a9e68424c89d
Syrine Bouslimi <syrine.bouslimi@esprit.tn>
2018-04-22 22:47:18 +0100

    commit

--- src/KidsFrontBundle/Controller/ParentController.php
+++ src/KidsFrontBundle/Controller/ParentController.php
@@ -23,11 +23,8 @@ class ParentController extends Controller
 
     }
 
-    public function afficherEtabParentAction()
+    public function afficherEtabParentAction($type)
     {
-
-
-
         $user = $this->container->get('security.token_storage')->getToken()->getUser();
         $idUser = $user->getId();
 
@@ -35,9 +32,13 @@ class ParentController extends Controller
 
 
         $allFavoris = $em->getRepository('UserBundle:UserEtablissementFavoris')->findBy(array('user' => $idUser));
+        if ($type == "all"){
+            $allEtab = $em->getRepository('UserBundle:Etablissement')->findAll();
 
-        $allEtab = $em->getRepository('UserBundle:Etablissement')->findAll();
+        } else{
+            $allEtab = $em->getRepository('UserBundle:Etablissement')->findBy(array('typeEtablissement'=>$type));
 
+        }
 
         foreach($allEtab as $e) {
 
