106efa8d3c335366cd7d7e4fc493d19789e62fe5
Syrine Bouslimi <syrine.bouslimi@esprit.tn>
2018-04-28 10:30:43 +0100

    fix comment display

--- src/UserBundle/Entity/Thread.php
+++ src/UserBundle/Entity/Thread.php
@@ -8,7 +8,7 @@ use FOS\CommentBundle\Entity\Thread as BaseThread;
 /**
  * @ORM\Entity
  * @ORM\ChangeTrackingPolicy("DEFERRED_EXPLICIT")
- * @ORM\Table(name="thread_comment")
+ * @ORM\Table(name="thread2")
  */
 class Thread extends BaseThread
 {
