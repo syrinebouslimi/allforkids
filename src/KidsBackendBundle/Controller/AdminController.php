a0fe361480d77fac79f57233696e5d1e016d9472
Syrine Bouslimi <syrine.bouslimi@esprit.tn>
2018-05-05 12:39:42 +0100

    commit

--- src/KidsBackendBundle/Controller/AdminController.php
+++ src/KidsBackendBundle/Controller/AdminController.php
@@ -14,6 +14,7 @@ class AdminController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $allNotif = $em->getRepository('MgiletNotificationBundle:NotifiableNotification')->findAll();
+
         return $this->render('KidsBackendBundle::Template_Admin.html.twig', array('notifiableNotifications' => $allNotif));
 
     }
